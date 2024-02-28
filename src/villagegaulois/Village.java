c94a9e545fad0988a91a2ff71907e4ea8fb192c1
youssefhosni123 <hosniyoussef36@gmail.com>
2024-02-28 11:48:35 +0100

    TP1progression

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -8,6 +8,46 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private static class  Marche {
+		private Etal[] etals;
+		
+		public Marche (int nbEtals)
+		{
+			etals= new Etal[nbEtals];
+			for (int i=0;i<etals.length;i++ )
+			{
+				etals[i]=new Etal();
+			}
+		}
+		 void utiliserEtal(int indiceEtal, Gaulois vendeur,String produit, int nbProduit)
+		{
+			 if (indiceEtal>=0 && indiceEtal<etals.length && !etals[indiceEtal].isEtalOccupe())
+			 {
+				 etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);	
+			 System.out.println(vendeur.getNom() + " s'est installé à l'étal " + indiceEtal + " avec " + nbProduit + " " + produit + "(s).");
+        } else {
+            System.out.println("Impossible d'utiliser l'étal " + indiceEtal + ". L'étal est déjà occupé ou l'indice est invalide.");
+        }
+		}
+		 int   trouveretatLibre()
+		 {
+			 for (int i=0;i<etals.length;i++)
+			 {
+				 if(!etals[i].isEtalOccupe())
+				 {
+					 return i;
+				 }
+				 
+			 }
+			 return -1;
+		 }
+		 Etal[] trouverEtals(String produit)
+		 {
+			 
+			 
+		 }
+		
+	}
 
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
